a2274560ab9bc9ee9deaad57f23ffc45b4b6fc58
Ryan Webster <ryan.h.webster@gmail.com>
2020-10-20 12:56:02 -0700

    completed bubble sort

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -7,8 +7,24 @@ function swap(array, idx1, idx2) {
 }
 
 function bubbleSort(array) {
-  // your code here
+  let swapped = true;
+
+  while(swapped) {
+    swapped = false;
+
+    for(let i = 1; i < array.length; i++) {
+      if(array[i] > array[i+1]) {
+        swap(array, i, i+1);
+        console.log(array);
+        swapped = true;
+      }
+    }
+  }
+  return array;
 }
+
+let array1 = [0, 4, 5, 6, 2, 9, 1, 3, 7];
+console.log(bubbleSort(array1));
 
 module.exports = {
   bubbleSort,
